a597b6e34bcd68689431855607c3cd5ee62f3eb2
abyjays <abyjays123@gmail.com>
2026-06-19 20:15:54 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Portfolio from './Portfolio'; 
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <Portfolio />
     </div>
   );
